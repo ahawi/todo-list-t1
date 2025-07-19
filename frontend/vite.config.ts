009118ea301e4757db93336f389183a178d9d3b4
ahawi <nastya.murlla@gmail.com>
2025-07-19 22:20:26 +0300

    Тестирование версель

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-list-t1/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
