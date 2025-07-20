89f8132c6ead3b0558387192422c0ecb90d21b60
ahawi <nastya.murlla@gmail.com>
2025-07-20 10:24:38 +0300

    Удаление бекенда из этого репозитория

--- backend/api/index.js
+++
@@ -1,3 +0,0 @@
-const app = require('../dist/app');
-
-module.exports = app;
--- backend/src/types/task.ts
+++
@@ -1,9 +0,0 @@
-export type Task = {
-  id: number
-  title: string
-  description?: string
-  priority: string
-  category: string
-  status: string
-  createdAt: string
-}
